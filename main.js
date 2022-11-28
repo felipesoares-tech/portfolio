70aa56510dbbd6d4a09c2561098089e0e14db9d0
Felipe <soares.felipe200@gmail.com>
2022-11-28 12:50:21 -0300

    construindo menu

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn')
+const navBar = document.getElementById('navigation')
 let menuOpen = false
 menuBtn.addEventListener('click',()=>{
     if(!menuOpen){
         menuBtn.classList.add('open')
         menuOpen = true
+        navBar.classList.toggle('active')
     }else{
         menuBtn.classList.remove('open')
         menuOpen = false
+        navBar.classList.toggle('active')
     }
 })
