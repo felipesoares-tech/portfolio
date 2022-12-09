74a211433558ddcb9936811d6a71385ed4b465f0
Felipe <soares.felipe200@gmail.com>
2022-12-08 23:23:57 -0300

    commit

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const menuBtn = document.getElementById('checkbox-menu')
 const navBar = document.getElementById('navigation')
+const spans = document.querySelectorAll('.prog')
 
-menuBtn.addEventListener('click',()=>{
-    if(menuBtn.checked == true){
+menuBtn.addEventListener('click', () => {
+    if (menuBtn.checked == true) {
         navBar.classList.toggle('active')
         document.body.style.overflowY = 'hidden'
-    }else{
+    } else {
         navBar.classList.toggle('active')
         document.body.style.overflowY = 'auto'
     }
+})
+
+$(document).on('scroll', function () {
+    if ($(this).scrollTop() >= $('#about').position().top) {
+        spans.forEach((item) => {
+            item.classList.remove('hideProgBar')
+        })
+    } else {
+        spans.forEach((item) => {
+            item.classList.add('hideProgBar')
+            
+        })
+
+    }
+
 })
