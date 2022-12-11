851eada94675fda2783fdaf9d542c609bdf53b7c
Felipe Soares <soares.felipe200@gmail.com>
2022-12-10 23:46:03 -0300

    incluindo tema light

--- main.js
+++ main.js
@@ -2,6 +2,65 @@ const menuBtn = document.getElementById('checkbox-menu')
 const navBar = document.getElementById('navigation')
 const spans = document.querySelectorAll('.prog')
 const items = document.getElementById('list-item-header')
+const rootVariables = document.querySelector(':root');
+const chkTheme = document.getElementById('chk');
+const htmlTag = document.getElementById('ht5')
+const GetTheme = JSON.parse(localStorage.getItem('PageTheme'))
+const tagDev = document.getElementById('dev-symb')
+
+const imgLamp = document.getElementById('lamp')
+const imgLike = document.getElementById('like')
+const imgUser = document.getElementById('user')
+const imgDesign = document.getElementById('design')
+
+console.log(GetTheme)
+
+if(GetTheme == 'LIGHT'){
+    htmlTag.classList.toggle('light-mode')
+    imgLamp.src = 'assets/img/lamp-light.png'
+    imgLike.src = 'assets/img/like-light.png'
+    imgUser.src = 'assets/img/user-light.png'
+    imgDesign.src = 'assets/img/design-light.png'
+    tagDev.src = 'assets/img/tag-light.png'
+}else{
+    imgLamp.src = 'assets/img/lamp.png'
+    imgLike.src = 'assets/img/like.png'
+    imgUser.src = 'assets/img/user.png'
+    imgDesign.src = 'assets/img/design.png'
+    tagDev.src = 'assets/img/tag.png'
+    
+}
+
+
+
+chkTheme.addEventListener('change', () => {
+	htmlTag.classList.toggle('light-mode')
+
+    var theme
+
+    if(htmlTag.classList.contains('light-mode')){
+        console.log('Light Mode')
+        theme = 'LIGHT'
+        imgLamp.src = 'assets/img/lamp-light.png'
+        imgLike.src = 'assets/img/like-light.png'
+        imgUser.src = 'assets/img/user-light.png'
+        imgDesign.src = 'assets/img/design-light.png'
+        tagDev.src = 'assets/img/tag-light.png'
+    }else{
+        console.log('Dark Mode')
+        theme = 'DARK'
+        imgLamp.src = 'assets/img/lamp.png'
+        imgLike.src = 'assets/img/like.png'
+        imgUser.src = 'assets/img/user.png'
+        imgDesign.src = 'assets/img/design.png'
+        tagDev.src = 'assets/img/tag.png'
+    }
+
+    //save to localStorage
+
+    localStorage.setItem('PageTheme', JSON.stringify(theme))
+
+});
 
 menuBtn.addEventListener('click', () => {
     if (menuBtn.checked == true) {
@@ -13,7 +72,6 @@ menuBtn.addEventListener('click', () => {
     }
 })
 
-
 $(document).on('scroll', function () {
     if ($(this).scrollTop() >= $('#about').position().top) {
         spans.forEach((item) => {
@@ -22,9 +80,6 @@ $(document).on('scroll', function () {
     } else {
         spans.forEach((item) => {
             item.classList.add('hideProgBar')
-            
         })
-
     }
-
 })
